ab9957b12bb6d645c010bebf09deac8c06ab7943
Megan <megantech@hotmail.com>
2023-08-01 15:29:56 +0000

    Add API endpoints in views.py using viewsets

--- myproject/hotfeet_reviews/views.py
+++ myproject/hotfeet_reviews/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Shoe, Review, Category
+from .serializers import ShoeSerializer, ReviewSerializer, CategorySerializer
 
-# Create your views here.
+class ShoeViewSet(viewsets.ModelViewSet):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
