d456ac5bda15d0835f11cf7d7364b3beab070d8c
Megan <megantech@hotmail.com>
2023-08-01 15:31:25 +0000

    Created urls.py and defined my routes wihin hotfeet_reviews

---
+++ myproject/hotfeet_reviews/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ShoeViewSet, ReviewViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'shoes', ShoeViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'categories', CategoryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
