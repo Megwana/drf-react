ec8b08d52188eae1c952d177604c334cad533e36
Megan <megantech@hotmail.com>
2023-08-01 16:21:29 +0000

    Add shoelist login and register links to navbar

--- myproject/frontend_react/src/components/NavBar.js
+++ myproject/frontend_react/src/components/NavBar.js
@@ -16,8 +16,9 @@ const NavBar = () => {
         <Navbar.Collapse id="navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/shoelist">Shoe List</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
