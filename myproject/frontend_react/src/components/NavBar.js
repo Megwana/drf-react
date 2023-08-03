8c414a96052b9f9dbbfa0040c6e39a2b7b4c675d
Megan <megantech@hotmail.com>
2023-08-03 22:08:51 +0000

    Add event listener to allow the navbar to collapse when the user clicks outside of the navbar

--- myproject/frontend_react/src/components/NavBar.js
+++ myproject/frontend_react/src/components/NavBar.js
@@ -1,26 +1,33 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import '../styles/navbar.css';
-import runningLogo from '../assets/running.png'
+import runningLogo from '../assets/running.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const node = useRef();
 
-  useEffect(() => {
-    function handleClick(e) {
-        if (node.current.contains(e.target)) {
-            return;
-        }
-        setIsOpen(false);
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
     }
+    // outside click 
+    setIsOpen(false);
+  };
 
+  useEffect(() => {
+    // add when mounted
     document.addEventListener("mousedown", handleClick);
-
+    // return function to be called when unmounted
     return () => {
-        document.removeEventListener("mousedown", handleClick);
+      document.removeEventListener("mousedown", handleClick);
     };
   }, []);
+
+  const handleToggle = () => setIsOpen(!isOpen);
 
   return (
     <Navbar ref={node} expand="lg" variant="dark" className="custom-navbar">
@@ -29,7 +36,9 @@ const NavBar = () => {
           <Navbar.Brand href="/"><b>Hot</b>Feet <img src={runningLogo} alt="Running Logo" className="running-logo" />
           </Navbar.Brand>
         </div>
-        <Navbar.Toggle onClick={() => setIsOpen(!isOpen)} aria-controls="navbar-nav" />
+        <Navbar.Toggle onClick={handleToggle} aria-controls="navbar-nav">
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </Navbar.Toggle>
         <Navbar.Collapse in={isOpen} id="navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
