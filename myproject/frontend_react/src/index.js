86a5da5d83564fb6626be87aab853c4b810c9704
Megan <megantech@hotmail.com>
2023-08-03 22:25:29 +0000

    Add favicon to website in all media friendly and device friendly sizings

--- myproject/frontend_react/src/index.js
+++ myproject/frontend_react/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
@@ -13,6 +14,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
